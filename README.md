a8e14e8822536f8cff43a94516d7af219bc4ab89
moniqueberry88 <moniqueberry88@gmail.com>
2018-09-27 10:44:22 -0500

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # influence_health_testing_project
+#to run command type 'mocha test_suite.js' from the project folder.
--- test_suite.js
+++ test_suite.js
@@ -15,9 +15,12 @@ describe("Test Suite", function () {
 
     after(function () {
 
+        // do something after test suite execution
+        // no matter if there are failed cases
 
     });
 
+    //creates selenium web driver and load the desired webpage before each test.
     beforeEach(function (done) {
 
         driver = new selenium.Builder()
@@ -27,42 +30,59 @@ describe("Test Suite", function () {
         driver.get('https://medseek-engineering.github.io/qa-engineer-test/').then(done);
     });
 
+    //closes webdriver after every test case.
     afterEach(function (done) {
         driver.quit().then(done);
-        
+
 
     });
 
+    //actual test case.
     it("Test-1", function (done) {
-        let name = 'Monique Berry';
-        let email = 'moniqueberry88@gmail.com'
-        let jobRole = 'Developer';
-        let workCompany = 'Unemployed';
 
+        //text to use in asserts methods
+        let confirmText1 = 'Join the fight against bad bugs';
+        let confirmText2 = 'Thank you for your interest';
+
+        //user data to be entered
+        let person = {
+            name: 'Monique Berry',
+            email: 'moniqueberry88@gmail.com',
+            jobRole: 'Developer',
+            workCompany: 'Unemployed'
+        };
+
+        //compare method 1
         driver.findElement(By.className('cover-heading')).getText().then(text => {
             console.log(text);
-            expect(text).equals('Join the fight against bad bugs');
-            
+            expect(text).equals(confirmText1);
+
         });
 
         driver.findElement(By.className('btn btn-lg btn-secondary')).click();
 
-
+        //compare method 2
         driver.findElement(By.className('cover-heading mb-5')).getText().then(text => {
             console.log(text);
-            expect(text).equals('Thank you for your interest');
+            expect(text).equals(confirmText2);
         });
 
-        driver.findElement(By.id('inputName')).sendKeys(name);
-        driver.findElement(By.id('inputEmail1')).sendKeys(email);
-        driver.findElement(By.id('inputJobTitle')).sendKeys(jobRole);
-        driver.findElement(By.xpath("//input[@placeholder='Company']")).sendKeys(workCompany);
+        //data entry point.
+        driver.findElement(By.id('inputName')).sendKeys(person.name);
+        driver.sleep(1000);
+        driver.findElement(By.id('inputEmail1')).sendKeys(person.email);
+        driver.sleep(1000);
+        driver.findElement(By.id('inputJobTitle')).sendKeys(person.jobRole);
+        driver.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Company']")).sendKeys(person.workCompany);
+        driver.sleep(1000);
         driver.findElement(By.id('inputIndustry')).click();
         driver.findElement(By.xpath("//select[@id='inputIndustry']/option[26]")).click();
+        driver.sleep(1000);
         driver.findElement(By.className('btn btn-lg btn-secondary')).click().then(done);
-        
+
     });
-    
+
 });
 
 
